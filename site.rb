bff126cb7530dcf2576ea39212a5b3d2f556b844
Nacho <nachofacello@gmail.com>
2008-04-27 21:31:34 -0700

    Random headers

--- site.rb
+++ site.rb
@@ -5,6 +5,10 @@ get '/' do
   haml :default
 end
 
+get '/img/randomheader.jpg' do
+  send_file 'public/img/headers/' + (rand(7) + 1).to_s + '.jpg'
+end
+
 get '/:anypage' do
   haml params[:anypage].to_sym
 end

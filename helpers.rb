026c459296fee6a1a16f59b4dcc9d8d4558aa1e5
Nacho <nacho@nucleartesuji.com>
2008-07-11 20:32:12 -0300

    Add helper method 'partial'

--- helpers.rb
+++ helpers.rb
@@ -3,7 +3,7 @@ require 'lib/augo'
 
 helpers do
   def make_link(url, title, description)
-    haml :_link, :locals => { :url => url, :title => title, :description => description }, :layout => false
+    partial :_link, :locals => { :url => url, :title => title, :description => description }
   end
 
   def round_td(round)
@@ -15,5 +15,10 @@ helpers do
     img = ""
     img = "<img src=\"/img/stone#{round.color}.png\">" if round.color
     "<td class=\"roundinfo\" style=\"color: #{color}\" title=\"#{opp.name} #{result}\" onMouseOver=\"highlightPlayer(#{player.ip}); highlightPlayer(#{opp.ip});\" onMouseOut=\"restorePlayer(#{player.ip}); restorePlayer(#{opp.ip});\" >#{img}#{opp.ip}#{wonlost}</td>"
+  end
+
+  def partial(file, options={})
+    options.merge!({ :layout => false })
+    haml file, options
   end
 end

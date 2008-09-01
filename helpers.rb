e70c80e8a210e9df99f04e1ab924ddc5533d0986
Nacho <nacho@nucleartesuji.com>
2008-09-01 13:17:11 -0300

    Cleaned up tournament page

--- helpers.rb
+++ helpers.rb
@@ -4,14 +4,14 @@ require 'lib/calendar'
 
 helpers do
   def round_td(round)
-    color = round.won? ? "green" : "red"
+    tdclass = round.won? ? "won" : "lost"
     opp = round.opponent
     player = round.player
     result = round.result.to_s
     wonlost = round.won? ? "+" : "-"
     img = ""
     img = "<img src=\"/img/stone#{round.color}.png\">" if round.color
-    "<td class=\"roundinfo\" style=\"color: #{color}\" title=\"#{opp.name} #{result}\" onMouseOver=\"highlightPlayer(#{player.ip}); highlightPlayer(#{opp.ip});\" onMouseOut=\"restorePlayer(#{player.ip}); restorePlayer(#{opp.ip});\" >#{img}#{opp.ip}#{wonlost}</td>"
+    "<td class=\"roundinfo #{tdclass}\" title=\"#{opp.name} #{result}\" onMouseOver=\"highlightPlayer(#{player.ip}); highlightPlayer(#{opp.ip});\" onMouseOut=\"restorePlayer(#{player.ip}); restorePlayer(#{opp.ip});\" >#{img}#{opp.ip}#{wonlost}</td>"
   end
 
   def partial(file, options={})

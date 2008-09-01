8d5bfe5075516a70d1cf060d74a0ab5fce157844
Nacho <nacho@nucleartesuji.com>
2008-09-01 13:27:35 -0300

    Changes to make it pass XHTML Strict validation.

--- helpers.rb
+++ helpers.rb
@@ -10,8 +10,8 @@ helpers do
     result = round.result.to_s
     wonlost = round.won? ? "+" : "-"
     img = ""
-    img = "<img src=\"/img/stone#{round.color}.png\">" if round.color
-    "<td class=\"roundinfo #{tdclass}\" title=\"#{opp.name} #{result}\" onMouseOver=\"highlightPlayer(#{player.ip}); highlightPlayer(#{opp.ip});\" onMouseOut=\"restorePlayer(#{player.ip}); restorePlayer(#{opp.ip});\" >#{img}#{opp.ip}#{wonlost}</td>"
+    img = "<img src=\"/img/stone#{round.color}.png\" alt=\"[#{round.color}]\"/>" if round.color
+    "<td class=\"roundinfo #{tdclass}\" title=\"#{opp.name} #{result}\">#{img}#{opp.ip}#{wonlost}</td>"
   end
 
   def partial(file, options={})

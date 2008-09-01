5ffbf264495095de1852c4a8ea7c46f7c462dab3
Nacho <nacho@nucleartesuji.com>
2008-09-01 17:54:54 -0300

    Some styling for tournament page

--- helpers.rb
+++ helpers.rb
@@ -10,8 +10,8 @@ helpers do
     result = round.result.to_s
     wonlost = round.won? ? "+" : "-"
     img = ""
-    img = "<img src=\"/img/stone#{round.color}.png\" alt=\"[#{round.color}]\"/>" if round.color
-    "<td class=\"roundinfo #{tdclass}\" title=\"#{opp.name} #{result}\">#{img}#{opp.ip}#{wonlost}</td>"
+    img = "<img src=\"/img/stone#{round.color}.png\" alt=\"[#{round.color}]\"/> " unless round.color.empty?
+    "<td class=\"round #{tdclass}\" title=\"#{opp.name} #{result}\">#{img}#{opp.ip}#{wonlost}</td>"
   end
 
   def partial(file, options={})

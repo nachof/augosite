2b5e50f742b83c10e390441847cafaeb5514d57b
Nacho <nacho@nucleartesuji.com>
2009-06-15 14:35:17 -0300

    Pixel-pushing

--- public/javascript/load_game.js
+++ public/javascript/load_game.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $('tr.player td.hasgame').click(function() {
     viewsgfurl = $(this).attr('viewsgf');
-    $.facebox('<iframe src="' + viewsgfurl + '" height="555" frameborder="0" width="422" scrolling="no" />')
+    $.facebox('<iframe src="' + viewsgfurl + '" height="555" frameborder="0" width="424" scrolling="no" />')
     //$.facebox(function() {
     //  $.get(viewsgfurl, function(data) {
     //      $.facebox(data)

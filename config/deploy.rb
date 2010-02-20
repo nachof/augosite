6f37902690c42c94e42b6bb325bfa5697fe188ba
Nacho Facello <nacho@nucleartesuji.com>
2010-02-20 17:49:52 -0200

    Whitespace #trivial

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 after "deploy:symlink", "deploy:symlink_configs"
 
-namespace :deploy do  
+namespace :deploy do
 
   desc "Start the app"
   task :start, :roles => :app do
@@ -13,12 +13,12 @@ namespace :deploy do
     sudo "a2dissite #{vhost}"
     sudo "/etc/init.d/apache2 reload"
   end
-  
+
   desc "Restart the app"
   task :restart, :roles => :app do
     run "touch #{current_path}/tmp/restart.txt"
   end
-  
+
   desc "Symlink config files"
   task :symlink_configs, :roles => :app, :except => { :no_symlink => true } do
     #run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
